95b52e95ae5653da34cee9567545f55779ac1de9
Andsirpa <andsirpa94@gmail.com>
2024-01-10 17:43:36 +0100

    add user's info

--- js/main.js
+++ js/main.js
@@ -1,1 +1,17 @@
-console.log('JS OK');
+
+const userName = document.getElementById('user-name')
+console.log(userName);
+
+const distance = document.getElementById('distance')
+console.log(distance);
+
+const userAge = document.getElementById('user-age')
+console.log(userAge);
+
+
+
+const printButton = document.getElementById('print-button')
+
+const priceKm = 0.21
+const basePrice = distance * priceKm
+console.log(basePrice);
