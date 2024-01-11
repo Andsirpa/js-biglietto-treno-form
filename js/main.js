9fe1b85680de847f889477fd6149ed0c9fefec4a
Andsirpa <andsirpa94@gmail.com>
2024-01-11 09:16:01 +0100

    add event

--- js/main.js
+++ js/main.js
@@ -13,23 +13,28 @@ console.log(userAge);
 
 const printButton = document.getElementById('print-button')
 
+const resultElement = document.getElementById('result-element')
 const priceKm = 0.21
 const basePrice = distance * priceKm
 console.log(basePrice);
 
 let discount = 0
 
-if (userAge == Minorenne) {
+if (userAge == 'Minorenne') {
     discount = 20
 }
 
-if (userAge == Over65) {
+if (userAge == 'Over65') {
     discount = 40
 }
 
-const discountPrice = basePrice * discount / 100
+const discountPrice = (basePrice * discount) / 100
 
 const finalPrice = basePrice - discountPrice
 
 const priceText = '€' + finalPrice
 console.log(priceText);
+
+printButton.addEventListener('click', function () {
+    resultElement.innerText = 'Il tuo biglietto costa' + priceText
+})
