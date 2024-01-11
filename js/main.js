6a6fafb738d9a83e4f4792eb7cfdca22d9815790
Andsirpa <andsirpa94@gmail.com>
2024-01-11 08:56:31 +0100

    add discount

--- js/main.js
+++ js/main.js
@@ -1,17 +1,35 @@
 
-const userName = document.getElementById('user-name')
+const userNameInput = document.getElementById('user-name')
+const userName = userNameInput.value
 console.log(userName);
 
-const distance = document.getElementById('distance')
+const distanceInput = document.getElementById('distance')
+const distance = parseInt(distanceInput.value);
 console.log(distance);
 
-const userAge = document.getElementById('user-age')
+const userAgeInput = document.getElementById('user-age')
+const userAge = userAgeInput.value
 console.log(userAge);
-
-
 
 const printButton = document.getElementById('print-button')
 
 const priceKm = 0.21
 const basePrice = distance * priceKm
 console.log(basePrice);
+
+let discount = 0
+
+if (userAge == Minorenne) {
+    discount = 20
+}
+
+if (userAge == Over65) {
+    discount = 40
+}
+
+const discountPrice = basePrice * discount / 100
+
+const finalPrice = basePrice - discountPrice
+
+const priceText = '€' + finalPrice
+console.log(priceText);
